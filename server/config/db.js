d7f0d250ea645720a229f3c09c419e07addf6b99
Rajneesh rana <90295886+rajneeshrana0@users.noreply.github.com>
2024-09-16 12:50:05 +0530

    seome change in config db file

--- server/config/db.js
+++ server/config/db.js
@@ -7,10 +7,7 @@ dotenv.config();
 // Function to connect to the database
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
